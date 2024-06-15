f4fc6036afb265f807dd877ddf035d7fe616254d
Ananya Agrawal <92045291+anagraw@users.noreply.github.com>
2024-06-15 22:26:56 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,44 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignInUp from "./pages/SignInUp";
+import Diagnostic from "./pages/Diagnostic";
+import Scanning from "./pages/Scanning";
+import Prescription from "./pages/Prescription";
+import Summary from "./pages/Summary";
+
+export default function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path='/sign'
+					element={<SignInUp />}
+				/>
+				<Route
+					path='/dashboard'
+					element={<Home />}
+				/>
+				<Route
+					path='/'
+					element={<Home />}
+				/>
+				<Route
+					path='/diagnostic'
+					element={<Diagnostic />}
+				/>
+				<Route
+					path='/scan'
+					element={<Scanning />}
+				/>
+				<Route
+					path='/presc'
+					element={<Prescription />}
+				/>
+				<Route
+					path='/summary'
+					element={<Summary />}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
+}
