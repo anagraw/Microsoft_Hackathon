b77d627651cd90222952bd9004a255400d0f010b
Ananya Agrawal <92045291+anagraw@users.noreply.github.com>
2024-07-22 09:50:32 +0530

    Update firebase-config.jsx
    Details Deleted
    contact ananyamazing@gmail.com

--- client/src/firebase-config.jsx
+++ client/src/firebase-config.jsx
@@ -1,17 +1,3 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-const firebaseConfig = {
-	apiKey: "AIzaSyBzVYRI6r1WsR-p4S2N1oH8rUcNBhCC2jM",
-	authDomain: "ms-hack-e0b6b.firebaseapp.com",
-	projectId: "ms-hack-e0b6b",
-	storageBucket: "ms-hack-e0b6b.appspot.com",
-	messagingSenderId: "772768837310",
-	appId: "1:772768837310:web:ad397b654b0cd2c0efb8ef",
-};
-
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+//Details removed after a month due to security reasons(Details Deleted on 22nd July)
+//Kindly contact ananyamazing@gmail.com incase API keys access is needed 
